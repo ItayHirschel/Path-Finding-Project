842b07d8bdec5104b62fdd861d537f04c87c2bc8
ItayHirschel <itaydh15989@gmail.com>
2022-11-16 19:25:40 +0200

    seperated screen from classes

--- Main.py
+++ Main.py
@@ -17,40 +17,34 @@ from dfs import DFS_scanner
 
     
 
-pygame.init()
-DEFAULT_FONT = pygame.font.SysFont( "cambria", FONT_SIZE)
 
 
-def draw_screen(screen, graph, Printables):
-    screen.fill(BLACK)
-    for printable in Printables:
+
+def draw_screen():
+    Printable.surface.fill(BLACK)
+    for printable in Printable.PRINTABLES:
         printable.blit()
     pygame.display.update()
 
 # MAINLOOP
 
 def mainloop(graph : MatrixGraph):
-    screen = graph.surface
+
     
-    WORK_MODE.surface = screen
-    WORK_MODE.font = DEFAULT_FONT
 
     alg_options = [
-        RadioButton.Option("BFS scan", BFS_scanner, 650,400),
-        RadioButton.Option("DFS scan", DFS_scanner, 650, 370)
+        RadioButton.CircleOption("BFS scan", BFS_scanner, 650, 400),
+        RadioButton.CircleOption("DFS scan", DFS_scanner, 650, 370)
     ]
 
-    ALGO_CHOICE = RadioButton(screen, DEFAULT_FONT, alg_options, 0)
+    ALGO_CHOICE = RadioButton(alg_options, 0)
 
 
-    RESET_BUTTON = PushButton(screen, DEFAULT_FONT, 700, 25, 180, 30, "reset graph", lambda : graph.graph_boot())
-    GRAPH_SIZE_TXT = TextBox(screen, 700, 100, DEFAULT_FONT, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], text = MatrixGlobals["graph size"])
-    DECREASE_SIZE_BUTTON = PushButton(screen, DEFAULT_FONT, 650, 100, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], "-", dec_size)
-    INCREASE_SIZE_BUTTON = PushButton(screen, DEFAULT_FONT, 750, 100, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], "+", inc_size)
+    RESET_BUTTON = PushButton(700, 25, 180, 30, "reset graph", lambda : graph.graph_boot())
+    GRAPH_SIZE_TXT = TextBox(700, 100, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], text = MatrixGlobals["graph size"])
+    DECREASE_SIZE_BUTTON = PushButton(650, 100, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], "-", dec_size)
+    INCREASE_SIZE_BUTTON = PushButton(750, 100, VisGlobals["graph size txt box"], VisGlobals["graph size txt box"], "+", inc_size)
 
-
-    PRINTABLES : list[Printable] = [WORK_MODE, RESET_BUTTON, graph, GRAPH_SIZE_TXT, DECREASE_SIZE_BUTTON, INCREASE_SIZE_BUTTON, ALGO_CHOICE]
-    CLICKABLES : list[Clickable]= [WORK_MODE, RESET_BUTTON, DECREASE_SIZE_BUTTON, INCREASE_SIZE_BUTTON, graph, ALGO_CHOICE]
 
     clock = pygame.time.Clock()
     run = True
@@ -97,18 +91,18 @@ def mainloop(graph : MatrixGraph):
             if event.type == pygame.MOUSEBUTTONDOWN:                                # MOUSE BUTTON
                 key_pressed = pygame.mouse.get_pressed()
                 if (key_pressed[MOUSE_KEYS.LEFT_KEY]):
-                    handel_mouse_left_key(CLICKABLES)
+                    handel_mouse_left_key()
                     
                 
                 
                 
-        draw_screen(screen, graph, PRINTABLES)
+        draw_screen()
 
         if type(algorithm) != ALGO_CHOICE.get_mode():
             algorithm = ALGO_CHOICE.get_mode()(graph)
 
         if not algo_running:
-            algorithm.update_queue()
+           algorithm.update_queue()
         
         #print(steps)
     pygame.quit()
@@ -118,10 +112,13 @@ if __name__ == "__main__":
 
     adjust_node_size()
     # SCREEN CREATION
-
+    pygame.init()
+    DEFAULT_FONT = pygame.font.SysFont( "cambria", FONT_SIZE)
     WIN = pygame.display.set_mode((PygameConstants["WIDTH"], PygameConstants["HEIGHT"]))
     pygame.display.set_caption(PygameConstants["SCREEN_TITLE"])
 
-    graph = MatrixGraph(WIN)
+    Printable.attach(WIN, DEFAULT_FONT)
+
+    graph = MatrixGraph()
     mainloop(graph)
 
